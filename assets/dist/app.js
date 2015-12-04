63d4884da0faebff93645d61174192f8500c3e0d
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-12-04 10:26:41 +0100

    feat(style.css): add custom stylesheet

---
+++ assets/dist/app.js
@@ -0,0 +1,39 @@
+
+var Landing = React.createClass({
+  render: function() {
+    return <section className ="background">
+    
+    </section >
+  }
+});
+
+var NavBar = React.createClass({
+  render: function() {
+    return <nav className = "navbar-fixed z-depth-1 amber darken">
+      <div className = "nav-wrapper">
+      <image src = "../../assets/images/gidi-logo.jpg"
+    className = "brand-logo left text-darken-3">
+      </image>
+      <ul className="right">
+      <li><a className = "btn-flat" href = "/about"> About GidiHots</a></li>
+      <li><a className = "btn-flat" href = "/subscribe"> Get Informed</a></li>
+      <li><a className = "btn-flat" href = "/contact"> Talk To Us </a></li>
+      </ul>
+    </div>
+    </nav>
+
+  }
+});
+
+'use strict';
+
+var MainPage = React.createClass({
+  render: function() {
+    return ( <section><NavBar />
+      <Landing/>
+      </section>)
+
+  }
+});
+
+ReactDOM.render( <MainPage /> , document.getElementById('main'));
