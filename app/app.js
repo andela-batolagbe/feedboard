a37f5e3fff096b4a8b3700f57b89cf4bc27fce7e
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-12-07 19:11:27 +0100

    add new components to render

--- app/app.js
+++ app/app.js
@@ -4,6 +4,10 @@ var MainPage = React.createClass({
   render: function() {
     return ( <section><NavBar />
       <Landing/>
+      <About/>
+      <Subscribe/>
+      <Contact/>
+      <Footer/>
       </section>)
 
   }
