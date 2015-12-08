921f89f076f8b0458975b038e026ecd06dc147fa
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-12-08 09:51:42 +0100

    chore: commit new minified and concat files

--- assets/dist/app.js
+++ assets/dist/app.js
@@ -187,9 +187,15 @@ var Footer = React.createClass({
     <div className="footer-copyright yellow darken-4">
             <div className="footer">
             © 2015 gidiHots
-            <a className="black-text right" href="https://github.com/andela-batolagbe">
-            Design with  <i className="material-icons red-text text-lighten-3">favourite</i> by
-            Bisoye from Andela </a>
+            <div className="right no-margin">
+            Design with  &nbsp;
+            <i className="material-icons prefix red-text text-dark-2">favorite</i> &nbsp; by &nbsp;
+            <a className="black-text" target="_blank" href="https://github.com/andela-batolagbe">
+            Bisoye &nbsp;</a>
+            from &nbsp;<a href="http://andela.com" target="_blank">
+            <img className="andela-footer" src ="../../assets/images/andela2.jpeg"></img>
+            </a>
+            </div>
             </div>
           </div>
         </footer>
