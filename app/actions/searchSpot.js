a7b20a9447c7ec421192e451a13d03c290e53246
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-12-11 18:41:15 +0100

    add api place cordinate request calls

--- app/actions/searchSpot.js
+++ app/actions/searchSpot.js
@@ -1,28 +1,46 @@
 import Flux from '../flux';
 
-const googleUrl = 'http://maps.google.com/maps/api/js?key=AIzaSyByBWK3irqo2wHU1mnFnx7r49j6JSsrNXI&libraries=places&output?json';
-
 class searchLocationSpots {
   constructor() {
     this.generateActions(
-      'searchSpotsSuccess',
-      'searchSpotsFail'
+      'updateLocation'
     );
   }
 
-  searchSpots(location) {
-    $.ajax({
-      type: 'POST',
-      url: googleUrl,
-      data: {location:location}
-    })
-      .done((data) => {
-        this.actions.searchSpotsSuccess(data.message);
-      })
-      .fail((err) => {
-        this.actions.searchSpotsError(err.responseJSON.message);
-      });
+  searchSuccess(response){
+    return response;
   }
-}
 
+  searchError(err){
+    return err;
+  }
+
+  searchSpots(place) {
+
+    let self = this;
+
+    let geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({
+      'address': place,
+      componentRestrictions: {
+        country: 'NG'
+      }
+    }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+      
+      let locationLatLng = {
+        lat: results[0].geometry.location.lat(), 
+        lng: results[0].geometry.location.lng()
+      }        
+       self.searchSuccess({ok: true, place: place, placeCord: locationLatLng});
+
+    }  else {
+        self.searchError({ok: false, status: status});
+      }
+    });
+
+  }
+
+}
 export default Flux.createActions(searchLocationSpots);
