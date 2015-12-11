8ee847635cf3f0f6ecec11e6f9816f6bc22fd338
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-12-11 18:42:15 +0100

    refactor store to listen to api dispatches

--- app/stores/spotStores.js
+++ app/stores/spotStores.js
@@ -1,19 +1,28 @@
 import Flux from '../flux';
 import searchLocationSpots from '../actions/searchSpot';
 
+
 class spotsStore {
   constructor() {
     this.bindActions(searchLocationSpots);
-    this.location= '';
-    this.spots = [];
+    this.place = 'a';
+    this.lat = 'a';
+    this.lng = 'a';
+    this.err = null;
   }
 
-  onSearchSpotsSuccess(data) {
-    this.spots = data;
-  }
+  onSearchSuccess(data) {
+    this.place = data.place;
+    this.lat = data.placeCord.lat;
+    this.lng = data.placeCord.lng;
+  } 
 
-  onSearchSpotsError(errorMessage) {
-    console.log(errorMessage);
+onSearchError(err) {
+    this.err = err.status + ' error fetching hotspots in your location, try again';
+    }
+
+  onUpdateLocation(event) {
+    this.location = event.target.value;
   }
 }
 
