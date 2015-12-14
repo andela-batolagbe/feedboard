508d39f4d4a041c3337544a9036c98bcf10c2c21
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-12-14 16:12:08 +0100

    remove unused imports

--- app/components/mainApp.js
+++ app/components/mainApp.js
@@ -1,9 +1,6 @@
  import React from 'react';
  import Navbar from './navbar';
  import Home from './home';
- import About from './about';
- import Subscribe from './subscribe';
- import Contact from './contact';
  import Footer from './footer';
 
  class App extends React.Component {
